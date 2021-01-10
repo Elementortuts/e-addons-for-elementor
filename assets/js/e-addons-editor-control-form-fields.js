27ee00704592e783cf618b0b518efb495c087e88
fra <fra@fra-SATELLITE-L70-B>
2021-01-10 11:34:56 +0100

    new control form_fields

---
+++ assets/js/e-addons-editor-control-form-fields.js
@@ -0,0 +1,58 @@
+var formFieldsItemView = elementor.modules.controls.BaseData.extend({
+    onReady: function () {
+
+        console.log('form_fields');
+        
+        var selectedElement = elementor.getCurrentElement();
+        if (selectedElement) {
+            var cid = selectedElement.model.cid;
+        } else {
+            var cid = jQuery('.elementor-navigator__item.elementor-editing').parent().data('model-cid');
+        }
+        console.log(cid);
+        
+        if (elementorFrontend.config.elements.data[cid]) {
+            var settings = elementorFrontend.config.elements.data[cid].attributes;
+            var fields = settings['form_fields'];
+            //console.log(fields);
+            
+            var options = '<option value="">No field</option>';                        
+            jQuery(fields.models).each(function(index,element){
+                var field_label = '[' + element.attributes.custom_id + '] (' + element.attributes.field_type + ')';
+                if (element.attributes.field_label) {
+                    if (element.attributes.field_label.length > 20) {
+                        field_label = element.attributes.field_label.substr(0, 20)  + '… ' + field_label;
+                    } else {
+                        field_label = element.attributes.field_label + ' ' + field_label;
+                    }
+                }
+                options += '<option value="'+element.attributes.custom_id+'">'+field_label+'</option>';
+            });
+
+                       
+            // single field
+            var select = this.$el.find('select');
+            var data_setting = select.data('setting');
+            var ids = settings[data_setting];
+            var is_select2 = false;
+            
+            if (select.hasClass("select2-hidden-accessible")) {
+                select.select2('destroy');
+                is_select2 = true;
+            }
+            select.html(options);
+            /*if (custom_id_input) {
+                // remove itself
+                select.find("option[value='" + custom_id_input.val() + "']").remove();
+            }*/
+            select.val(ids);
+            if (is_select2) {
+                select.select2();
+            }
+        }
+
+
+    },
+});
+
+elementor.addControlView('form_fields', formFieldsItemView);
---
+++ core/controls/form-fields.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace EAddonsForElementor\Core\Controls;
+
+use \Elementor\Modules\DynamicTags\Module as TagsModule;
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * FileSelect control.
+ *
+ * A control for selecting any type of files.
+ *
+ * @since 1.0.0
+ */
+class Form_Fields extends \Elementor\Control_Select {
+    
+    const CONTROL_TYPE = 'form_fields';
+
+    /**
+     * Get control type.
+     *
+     * Retrieve the control type, in this case `form_fields`.
+     *
+     * @since 1.0.0
+     * @access public
+     *
+     * @return string Control type.
+     */
+    public function get_type() {
+        return self::CONTROL_TYPE;
+    }
+
+    /**
+     * Enqueue control scripts and styles.
+     *
+     * Used to register and enqueue custom scripts and styles
+     * for this control.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function enqueue() {
+        wp_enqueue_style('e-addons-editor-control-form-fields');
+        // Scripts
+        wp_enqueue_script('e-addons-editor-control-form-fields');
+    }
+    
+    /**
+     * Render e-query control output in the editor.
+     *
+     * Used to generate the control HTML in the editor using Underscore JS
+     * template. The variables for the class are available using `data` JS
+     * object.
+     *
+     * @since 1.0.1
+     * @access public
+     */
+    public function content_template() {
+        ob_start();
+        parent::content_template();
+        $template = ob_get_clean();
+        $template = str_replace('<select ', '<# var multiple = ( data.multiple ) ? \'class="elementor-select2" type="select2" multiple\' : \'\'; #><select {{ multiple }} ', $template);
+        echo $template;
+    }
+
+}
