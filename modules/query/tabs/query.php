895ca2b7ca9c2f2f38f77d7adbb0a23403863346
fra <fra@fra-SATELLITE-L70-B>
2021-02-02 20:09:12 +0100

    query no results

--- modules/query/tabs/query.php
+++ modules/query/tabs/query.php
@@ -21,40 +21,4 @@ class Query extends Base_Tab {
         return __(ucfirst($this->get_name()), 'elementor');
     }
 
-    protected function register_tab_controls() {
-        
-        $element = $this;
-        if (is_subclass_of($this, 'Elementor\Element_Base')) {
-            $element = $this->parent;
-        }
-
-        $element->start_controls_section(
-                'e_section_' . $this->get_name() . '_advanced', [
-            'tab' => 'e_' . $this->get_name(),
-            'label' => __(ucfirst($this->get_name()), 'elementor'),
-                ]
-        );
-        $this->add_control(
-            'skin_test',
-            [
-                'type' => Controls_Manager::RAW_HTML,
-                'show_label' => false,
-                'raw' => '<i class="eaddicon-skin eadd-queryviews-gridtofullscreen"></i> TEST',
-            ]
-        );
-        $element->end_controls_section();
-
-        $icon = '<i class="fa fa-life-ring pull-right ml-1" aria-hidden="true"></i> ';
-        $element->start_controls_section(
-                'e_section_' . $this->get_name() . '_fallback', [
-            'tab' => 'e_' . $this->get_name(),
-            'label' => $icon . __('Fallback', 'elementor'),
-            'condition' => [
-                'e_' . $this->get_name() . '_mode!' => '',
-            ]
-                ]
-        );
-        $element->end_controls_section();
-    }
-
 }
