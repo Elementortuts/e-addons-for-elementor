9ffe8fc0c5b2967843fbd8498cf30ec00b46acbe
Poglie <poglie@nerds.farm>
2021-02-12 11:10:43 +0100

    icon section

--- modules/query/skins/table.php
+++ modules/query/skins/table.php
@@ -50,7 +50,7 @@ class Table extends Base {
 
         $this->start_controls_section(
                 'section_table', [
-            'label' => '<i class="eaddicon eadd-queryviews-table"></i> ' . __('Table', 'e-addons'),
+            'label' => '<i class="eaddicon eadd-skin-table"></i> ' . __('Table', 'e-addons'),
             'tab' => Controls_Manager::TAB_CONTENT,
                 ]
         );
